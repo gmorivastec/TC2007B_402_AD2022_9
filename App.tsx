5a46befef56da4da455c2c601557bf897cc18066
Guillermo Rivas <grivas@pop-os.localdomain>
2022-10-19 16:14:06 -0500

    clase de oct 19

--- App.tsx
+++ App.tsx
@@ -3,6 +3,12 @@ import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button, ActivityIndicator } from 'react-native';
 
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import MapView, {PROVIDER_GOOGLE, enableLatestRenderer, LatLng, Marker } from 'react-native-maps';
+
+// hay más de 1 renderer de mapas de google
+// renderer - el código que dibuja el mapa en pantalla
+enableLatestRenderer();
 
 function App() {
 
@@ -10,6 +16,10 @@ function App() {
   const[password, setPassword] = useState("");
   const[initializing, setInitializing] = useState(true);
   const[user, setUser] = useState();
+  const[nombre, setNombre] = useState("");
+  const[peso, setPeso] = useState(0.0);
+  var lista: LatLng[] = [];
+  const [markers, setMarkers] = useState(lista);
 
   // funcion para lidiar con cambio de usuario conectado
   function onAuthStateChanged(user: any){
@@ -29,6 +39,20 @@ function App() {
     return funcionDesuscribir;
   }, []);
 
+  useEffect(() => {
+
+    const funcionDesuscribir = firestore()
+    .collection("Perritos")
+    .onSnapshot(perritos => {
+      console.log("SUSCRIPCION!");
+      perritos.docs.forEach(perritoActual => {        
+        console.log("nombre: " + perritoActual.data().nombre + " peso: " + perritoActual.data().peso);
+      });
+    });
+
+    return funcionDesuscribir;
+  }, []);
+
   if(initializing){
     return (
       <ActivityIndicator size={"large"}/>
@@ -37,8 +61,8 @@ function App() {
 
   return (
     <View>
-      <Text style={{fontSize: 40, fontWeight: 'bold', color: 'red'}}>HOLA CAMBIO RÁPIDO</Text>
       <Text style={{fontSize: 20, fontWeight: 'bold', color: 'red'}}>HOLA CAMBIO RÁPIDO</Text>
+      <Text style={{fontSize: 10, fontWeight: 'bold', color: 'red'}}>HOLA CAMBIO RÁPIDO</Text>
       <View style={styles.contenedor}>
         <Text style={styles.texto1}>A</Text>
         <Text style={styles.texto2}>B</Text>
@@ -100,6 +124,69 @@ function App() {
           }}
         />
       </View>
+      <TextInput 
+        placeholder='nombre del perrito'
+        onChangeText={(text) => {
+          setNombre(text);
+        }}
+      />
+      <TextInput 
+        placeholder='peso del perrito'
+        onChangeText={(text) => {
+          setPeso(parseFloat(text));
+        }}
+      />
+      <View style={styles.botoncito}>
+        <Button 
+          title='agregar nuevo documento a colección'
+          onPress={() => {
+            firestore()
+            .collection("Perritos")
+            .add({
+              nombre: nombre,
+              peso: peso,
+            })
+            .then(() => {
+              console.log("perrito agregado");
+            });
+          }}
+        />
+      </View>
+      <View style={styles.botoncito}>
+        <Button 
+          title='hacer query a colección'
+          onPress={() => {
+            firestore()
+            .collection("Perritos")
+            .get()
+            .then(perritos => {
+
+              perritos.docs.forEach(perritoActual => {
+                console.log("nombre: " + perritoActual.data().nombre + " peso: " + perritoActual.data().peso);
+              });
+            });
+          }}
+        />
+      </View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.mapContainer}
+        region={{
+          latitude: 20.734804,
+          longitude:-103.457384,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+
+        onPress={event => {
+          console.log(event.nativeEvent.coordinate);
+          setMarkers(markers.concat(event.nativeEvent.coordinate));
+        }}
+      >
+        {
+          markers.length > 0 && markers.map((actual, i) => (<Marker coordinate={actual} key={i} />))
+        }
+      </MapView>
     </View>
   );
 }
@@ -112,7 +199,7 @@ const styles = StyleSheet.create({
 
   contenedor: {
     flexDirection: 'row',
-    height: 200
+    height: 20
   },
   texto1: {
     backgroundColor: '#fcad03',
@@ -128,8 +215,15 @@ const styles = StyleSheet.create({
   },
   botoncito: {
     padding: 5
+  },
+  mapContainer: {
+    height: "100%",
+    width: "100%",
+  },
+  map: {
+    height: "100%",
+    width: "100%",
   }
-
 });
 
 export default App;
